0c22930f2d57dc50f73f061230aa0e27214e35ea
kevin-guerrero <kevin.guerrero.7e6@itb.cat>
2026-05-17 21:18:53 +0200

    feat: quitar el JS del index i actualitzar estils

--- public/index.php
+++ public/index.php
@@ -19,24 +19,18 @@ $app->get('/', function (Request $request, Response $response) {
     $cardsHtml = "";
 
     foreach ($llistaMusics as $music) {
-
-        $nom = htmlspecialchars($music['grup_nom'], ENT_QUOTES, 'UTF-8');
-        $desc = htmlspecialchars($music['grup_descripcio'], ENT_QUOTES, 'UTF-8');
-        $img = htmlspecialchars($music['img_url'], ENT_QUOTES, 'UTF-8');
-        $video = htmlspecialchars($music['video_url'], ENT_QUOTES, 'UTF-8');
+        $id   = (int)$music['grup_id'];
+        $nom  = htmlspecialchars($music['grup_nom'], ENT_QUOTES, 'UTF-8');
+        $img  = htmlspecialchars($music['img_url'], ENT_QUOTES, 'UTF-8');
 
         $cardsHtml .= "
-            <div class='card'
-                data-nom=\"$nom\"
-                data-desc=\"$desc\"
-                data-video=\"$video\">
-
+            <a class='card' href='detail.php?id=$id'>
                 <img src=\"$img\" alt=\"$nom\">
-
-                <div class='container'>
-                    <h4><b>$nom</b></h4>
+                <div class='card-label'>
+                    <span class='tag'>Grup Musical</span>
+                    <h4>$nom</h4>
                 </div>
-            </div>
+            </a>
         ";
     }
 
@@ -46,52 +40,19 @@ $app->get('/', function (Request $request, Response $response) {
         <head>
             <meta charset='UTF-8'>
             <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <title>Pàgina Principal</title>
+            <title>Grups Musicals</title>
             <link rel='stylesheet' href='assets/css/style.css'>
         </head>
         <body>
 
+            <header>
+                <h1>Grups Musicals</h1>
+                <span>Descobreix els artistes</span>
+            </header>
+
             <div class='grid-container'>
                 $cardsHtml
             </div>
-
-            <div id='details-modal'>
-                <div class='modal-content'>
-                    <span class='close' onclick='closeModal()'>&times;</span>
-                    <h2 id='modal-title'></h2>
-                    <p id='modal-desc'></p>
-                    <a id='modal-video' href='' target='_blank'>Veure Vídeo en YouTube</a>
-                </div>
-            </div>
-
-            <script>
-                document.querySelectorAll('.card').forEach(card => {
-                    card.addEventListener('click', function () {
-                        showDetails(
-                            this.dataset.nom,
-                            this.dataset.desc,
-                            this.dataset.video
-                        );
-                    });
-                });
-
-                function showDetails(nom, desc, video) {
-                    document.getElementById('modal-title').innerText = nom;
-                    document.getElementById('modal-desc').innerText = desc;
-                    document.getElementById('modal-video').href = video;
-                    document.getElementById('details-modal').style.display = 'block';
-                }
-
-                function closeModal() {
-                    document.getElementById('details-modal').style.display = 'none';
-                }
-
-                window.onclick = function(event) {
-                    if (event.target.id === 'details-modal') {
-                        closeModal();
-                    }
-                }
-            </script>
 
         </body>
         </html>
